a8ecd1711d7efca87103dba040072f016949c247
Lahiouel <dm_lahiouel@esi.dz>
2018-12-19 02:13:22 +0100

    Update form-jquery-validation.js

--- js/form-jquery-validation.js
+++ js/form-jquery-validation.js
@@ -38,66 +38,23 @@ $( document ).ready(function() {
 	$("#validate").on("click",function(event) {
 	event.preventDefault();
 	    //alert($("#nom").val().length);
-	   if ($("#nom").val().length < 5)
+	   if ($("#nom").val().length < 5 || $("#prenom").val().length < 5 || $("#dateN").val().length < 5 || $("#adr").val().length < 5 || $("#mail").val().length < 5 )
     {
 	   $('#myModal').modal("show");
-	   /*$("#resultat").attr('style','display:none');
-	   $("#error").attr('style','display:block');
-	   $("#error").html("La saisie du nom est obligatoire");*/
 	   
 	}
-	else if ($("#prenom").val().length < 5)
-    {
-	   $("#resultat").attr('style','display:none');
-	   $("#error").attr('style','display:block');
-	   $("#error").html("La saisie du prénom est obligatoire");
-	   
-	}
-	else if ($("#dateN").val().length < 5)
-    {
-	   $("#resultat").attr('style','display:none');
-	   $("#error").attr('style','display:block');
-	   $("#error").html("La saisie de la date est obligatoire");
-	   
-	}
-	else if ($("#adr").val().length < 5)
-    {
-	  $("#resultat").attr('style','display:none');
-	   $("#error").attr('style','display:block');
-	   $("#error").html("La saisie de l'adresse est obligatoire");
-	   
-	}
-	else if ($("#mail").val().length < 5)
-    {
-	   $("#resultat").attr('style','display:none');
-	   $("#error").attr('style','display:block');
-	   $("#error").html("La saisie du mail est obligatoire");
-	   
-	}
+	
 	else
-	{
-	 /*   $("#error").attr('style','display:none');
-	   $("#resultat").attr('style','display:block');
-	   $("#resultat").html("Bienvenue " + $("#nom").val() + "  " + $("#prenom").val());*/
-	   
-	   
-	   
+	{	   
 	    $('#titre').html("Bienvenue " + $("#nom").val());
 	    $('#text').html("Vous etes né le " + $("#dateN").val());
-		$('.modal-body').append("<img src='https://maps.googleapis.com/maps/api/staticmap?markers="+$("#adr").val()+"&zoom=10&size=400x300&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg' >");
-		
-		
-		
+            $('.modal-body').append("<img src='https://maps.googleapis.com/maps/api/staticmap?markers="+$("#adr").val()+"&zoom=10&size=400x300&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg' >");	
 	    $('#myModal').modal("show");
 	}
 	
 	
 	
 	});
-	
-	/*$("#dateN").datepicker({
-	    dateFormat: "yy/mm/dd"
-	});*/
 	
 	
 	
